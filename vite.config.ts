1cc548331021da67fe0d5af6f2b7c7d7164abe30
qianphong <qianphong@163.com>
2023-03-16 17:43:16 +0800

    chore: add visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './node_modules/.cache/visualizer/stats.html',
+      open: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': `${resolve(__dirname, './src')}`,
